748b210c99c04ca856be76e6c5af32383f19d63f
Pedro Vilas Bôas <phvb20@gmail.com>
2024-05-04 01:44:24 -0300

    code indentation | (now yes end of project for now)

--- script.js
+++ script.js
@@ -1,20 +1,24 @@
-const shortBtn = document.getElementById('short-btn');
-const reloadBtn = document.getElementById('reload-btn');
+const shortBtn = document.getElementById("short-btn");
+const reloadBtn = document.getElementById("reload-btn");
 
-shortBtn.addEventListener('click', shortenUrl);
+shortBtn.addEventListener("click", shortenUrl);
 
 function shortenUrl() {
-  let longUrl = document.getElementById('originalUrl').value;
-  let apiUrl = 'https://tinyurl.com/api-create.php?url=' + encodeURIComponent(longUrl);
-  shortnedUrlTextArea = document.getElementById('shortnedUrl');
+  let longUrl = document.getElementById("originalUrl").value;
+  let apiUrl =
+    "https://tinyurl.com/api-create.php?url=" + encodeURIComponent(longUrl);
+  shortnedUrlTextArea = document.getElementById("shortnedUrl");
 
-  fetch(apiUrl).then((response) => response.text()).then(data => {
-    shortnedUrlTextArea.value = data;
-  }).catch((error) => {
-    shortnedUrlTextArea.value = 'Error: Unable to shorten URL! ' + error;
-  });
+  fetch(apiUrl)
+    .then((response) => response.text())
+    .then((data) => {
+      shortnedUrlTextArea.value = data;
+    })
+    .catch((error) => {
+      shortnedUrlTextArea.value = "Error: Unable to shorten URL! " + error;
+    });
 }
 
-reloadBtn.addEventListener('click', () => {
-    location.reload();
+reloadBtn.addEventListener("click", () => {
+  location.reload();
 });

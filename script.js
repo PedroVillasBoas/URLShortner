1e9d637745202213ed8ac59d70ca3857364d9500
Pedro Vilas Bôas <phvb20@gmail.com>
2024-05-04 02:02:58 -0300

    added comments to js code

--- script.js
+++ script.js
@@ -1,24 +1,31 @@
+// Code for URL shortner | Made by Pedro Vilas Bôas
+
+// Get the elements from the HTML
 const shortBtn = document.getElementById("short-btn");
 const reloadBtn = document.getElementById("reload-btn");
 
+// Add event listener to the button
 shortBtn.addEventListener("click", shortenUrl);
 
+// Function to shorten the URL
 function shortenUrl() {
-  let longUrl = document.getElementById("originalUrl").value;
+  let longUrl = document.getElementById("originalUrl").value; // Get the URL from the input field
   let apiUrl =
     "https://tinyurl.com/api-create.php?url=" + encodeURIComponent(longUrl);
-  shortnedUrlTextArea = document.getElementById("shortnedUrl");
+  shortnedUrlTextArea = document.getElementById("shortnedUrl"); // Get the textarea to display the shortned URL
 
-  fetch(apiUrl)
-    .then((response) => response.text())
-    .then((data) => {
-      shortnedUrlTextArea.value = data;
+  // Fetch the API
+  fetch(apiUrl) 
+    .then((response) => response.text()) // Get the response as text
+    .then((data) => {                    // Handle the data
+      shortnedUrlTextArea.value = data;  // Display the shortned URL in the textarea
     })
-    .catch((error) => {
+    .catch((error) => {                  // Handle the error
       shortnedUrlTextArea.value = "Error: Unable to shorten URL! " + error;
     });
 }
 
+// Add event listener to the reload button
 reloadBtn.addEventListener("click", () => {
   location.reload();
 });
